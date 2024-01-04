c02034c8c9bb166568684824f34f5b4f3c5e5f10
rajatjhamnani <rajatjhamnani@gmail.com>
2024-01-04 20:55:26 +0530

    Making apps error proof

--- src/Components/UI/fetchingMovieData/MovieData.js
+++ src/Components/UI/fetchingMovieData/MovieData.js
@@ -1,25 +1,42 @@
-import React, { useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 
 const MovieData = (props) => {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-
-  const fetchMovieHandler = async () => {
-    setIsLoading(true);
-    const response = await fetch("https://swapi.dev/api/films");
-    const data = await response.json();
-
-    const transformedData = data.results.map((movie) => {
-      return {
-        id: movie.episode_id,
-        title: movie.title,
-        openingText: movie.opening_crawl,
-        releaseDate: movie.release_date,
-      };
-    });
-    setMovies(transformedData);
+  const [error, setError] = useState(null);
+  function stopHandler() {
     setIsLoading(false);
-  };
+    setError(!error);
+  }
+
+  const fetchMovieHandler = useCallback(async () => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch("https://swapi.dev/api/films");
+      if (!response.ok) {
+        throw new Error("something went wrong ...Retrying");
+      }
+      const data = await response.json();
+
+      const transformedData = data.results.map((movie) => {
+        return {
+          id: movie.episode_id,
+          title: movie.title,
+          openingText: movie.opening_crawl,
+          releaseDate: movie.release_date,
+        };
+      });
+      setMovies(transformedData);
+    } catch (error) {
+      setError(error.message);
+    }
+    setIsLoading(false);
+  }, []);
+
+  useEffect(() => {
+    fetchMovieHandler();
+  }, [fetchMovieHandler]);
   return (
     <>
       <section>
@@ -39,8 +56,10 @@ const MovieData = (props) => {
           ))}
         </section>
       )}
-      {!isLoading && movies.length === 0 && <p>Found no movies</p>}
+      {!isLoading && movies.length === 0 && !error && <p>Found no movies</p>}
       {isLoading && <p>Loading...</p>}
+      {!isLoading && error && <p>{error}</p>}
+      <button onClick={stopHandler}>Stop</button>
     </>
   );
 };
